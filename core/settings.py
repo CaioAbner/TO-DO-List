ec65b47f0c4f27904148a375455466ebea34e94e
Caio Abner <caioabnerdeoliveirasantos@gmail.com>
2026-04-23 07:29:59 -0300

    Adição da imagem de preview

--- core/settings.py
+++ core/settings.py
@@ -122,4 +122,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'https://to-do-tan-seven.vercel.app/',
+]
